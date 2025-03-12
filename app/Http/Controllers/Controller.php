d6ba1e54a9ba07decf05dfdd419b5ead0c39e131
Alan Zuñiga <alan.zuniga@alumnos.udg.mx>
2025-03-12 00:05:15 -0600

    elimine -> foundation en controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
-
-class Controller extends BaseController
-{
-    use AuthorizesRequests, ValidatesRequests;
-}
-
 abstract class Controller
 {
     //
 }
+
+
+
+
+
+
+//use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+//use Illuminate\Foundation\Validation\ValidatesRequests;
+//use Illuminate\Routing\Controller as BaseController;
+
+//class Controller extends BaseController
+//{
+  //  use AuthorizesRequests, ValidatesRequests;
+//}
+
