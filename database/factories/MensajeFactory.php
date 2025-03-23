997d73bc55f6fdcfd0b3c5d1e8c7092426cb0247
Alan Zuñiga <alan.zuniga@alumnos.udg.mx>
2025-03-23 13:05:24 -0600

    Mensaje.php, MensajeFactory, testing -> MensajesControllerTest.php

---
+++ database/factories/MensajeFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Mensaje>
+ */
+class MensajeFactory extends Factory
+{
+
+    public function definition(): array
+    {
+        return [
+            'nombre' => fake()->name(),
+            'correo' => fake()->unique()->safeEmail(),
+            'mensaje' => fake()->sentence(),
+        ];
+    }
+}
--- tests/Feature/MensajesControllerTest.php
+++ tests/Feature/MensajesControllerTest.php
@@ -2,21 +2,68 @@
 
 namespace Tests\Feature;
 
+use App\Models\Mensaje;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class MensajesControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      */
     public function test_muestra_listado_mensajes(): void
     {
-        //response()
-        $response = $this->get('/');
-        $response->assertSee('Listado de mensajes');
+        // $response = $this->get(route('mensajes.index'));
+        $response = $this->get('/mensajes');
+        $response->assertSee('Listado de Mensajes');
 
         $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function muestra_formulario_crear_mensaje(): void
+    {
+        $response = $this->get('/mensajes/create');
+        $response->assertSee('Crear Mensaje')
+            ->assertSeeHtml('name="nombre"', false);
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function crear_nuevo_mensaje(): void
+    {
+        //Dado
+        $mensaje = Mensaje::factory()->make();
+
+        //Acción
+        $response = $this->post('/mensajes', $mensaje->toArray());
+        
+        //Expectativa
+        $this->assertDatabaseHas('mensajes', $mensaje->toArray());
+        //$response->assertStatus(302);
+        $response->assertRedirect('/mensajes');
+    }
+
+    /** @test */
+    public function muestra_formulario_editar_mensaje(): void
+    {
+        //Dado
+        $mensaje = Mensaje::factory()->create();
+
+        //Acción
+        // $response = $this->get("/mensajes/{$mensaje->id}/edit");
+        $response = $this->get(route('mensajes.edit', $mensaje->id));
+
+        //Expectativa
+        $response->assertSee('Editar mensaje')
+            ->assertSeeHtml($mensaje->nombre)
+            ->assertSeeHtml($mensaje->correo)
+            ->assertSeeHtml($mensaje->mensaje)
+            ->assertStatus(200);
+
     }
 }
